d876fe939d79418b64b221275857cb747d7449bd
jenniekibiri <josphatje@gmail.com>
2020-08-31 23:14:56 +0300

    implement twilio for otp

--- index.js
+++ index.js
@@ -44,11 +44,111 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get('/home', (req, res)=>{
+    res.status(200).send({
+        message: "You are on Homepage",
+        info: {
+            login: "Send verification code through /login . It contains two params i.e. phonenumber and channel(sms/call)",
+            verify: "Verify the recieved code through /verify . It contains two params i.e. phonenumber and code"
+        }
+    })
+})
+
+
+
+
+
+
+
+
+const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
+
+// /login
+//     - phone number
+//     - channel (sms/call)
+
+// /verify
+//     - phone number
+//     - code
+
+
+app.get('/home', (req, res)=>{
+    res.status(200).send({
+        message: "You are on Homepage",
+        info: {
+            login: "Send verification code through /login . It contains two params i.e. phonenumber and channel(sms/call)",
+            verify: "Verify the recieved code through /verify . It contains two params i.e. phonenumber and code"
+        }
+    })
+})
+
+
+// Login Endpoint
+app.get('/login', (req,res) => {
+     if (req.body.phone) {
+        client
+        .verify
+        .services(process.env.SERVICE_ID)
+        .verifications
+        .create({
+            to: `+${req.body.phone}`,
+            channel: req.query.channel==='call' ? 'call' : 'sms' 
+        })
+        .then(data => {
+            res.status(200).send({
+                message: "Verification is sent!!",
+                phone: req.body.phone,
+                data
+            })
+        }) 
+     } else {
+        res.status(400).send({
+            message: "Wrong phone number :(",
+            phone: req.body.phonenumber,
+            data
+        })
+     }
+})
+
+// Verify Endpoint
+app.get('/verify', (req, res) => {
+    if (req.query.phone && (req.query.code).length === 6) {
+        client
+            .verify
+            .services(process.env.SERVICE_ID)
+            .verificationChecks
+            .create({
+                to: `+${req.query.phone}`,
+                code: req.query.code
+            })
+            .then(data => {
+              console.log(data)
+                if (data.status === "approved") {
+                    res.status(200).send({
+                        message: "User is Verified!!",
+                        data
+                    })
+                }
+            })
+    } else {
+        res.status(400).send({
+            message: "Wrong phone number or code :(",
+            phone: req.query.phone,
+            data
+        })
+    }
+})
+
+
+
+
 app.use((err, req, res) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: "Unauthorized " });
   }
 });
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
